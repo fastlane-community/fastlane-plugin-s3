bc0d5af5c4f270b9fb53672de138a44755b63978
Josh Holtz <josh@rokkincat.com>
2016-06-03 16:44:36 -0500

    It works, woot woot

---
+++ example/fastlane/README.md
@@ -0,0 +1,33 @@
+fastlane documentation
+================
+# Installation
+```
+sudo gem install fastlane
+```
+# Available Actions
+### ipa21
+```
+fastlane ipa21
+```
+
+### ipa24
+```
+fastlane ipa24
+```
+
+### apk1
+```
+fastlane apk1
+```
+
+### apk2
+```
+fastlane apk2
+```
+
+
+----
+
+This README.md is auto-generated and will be re-generated every time to run [fastlane](https://fastlane.tools).
+More information about fastlane can be found on [https://fastlane.tools](https://fastlane.tools).
+The documentation of fastlane can be found on [GitHub](https://github.com/fastlane/fastlane/tree/master/fastlane).
--- lib/fastlane/plugin/s3/helper/s3_helper.rb
+++ lib/fastlane/plugin/s3/helper/s3_helper.rb
@@ -7,6 +7,27 @@ module Fastlane
       def self.show_message
         UI.message("Hello from the s3 plugin helper!")
       end
+
+      #
+      # Taken from https://github.com/fastlane/fastlane/blob/9c0494ef5e7d71afc51c73fe0b141b02e8991d9c/fastlane/lib/fastlane/erb_template_helper.rb
+      # Because I need to load from my plugin gem (not main fastlane gem)
+      #
+      require "erb"
+      def self.load(template_name)
+        path = "#{Helper.gem_path('fastlane-plugin-s3')}/lib/assets/#{template_name}.erb"
+        load_from_path(path)
+      end
+
+      def self.load_from_path(template_filepath)
+        unless File.exist?(template_filepath)
+          UI.user_error!("Could not find Template at path '#{template_filepath}'")
+        end
+        File.read(template_filepath)
+      end
+
+      def self.render(template, template_vars_hash)
+        Fastlane::ErbalT.new(template_vars_hash).render(template)
+      end
     end
   end
 end
